e9b45c0a47966fba8cb199f5c7a2484a584be9c9
daveseco7 <david.m.fontes@gmail.com>
2017-02-12 18:33:33 +0000

    Added different suite's for each function

--- bench.js
+++ bench.js
@@ -1,5 +1,6 @@
 'use strict';
 /* global bench */
+/* global suite */
 const fs = require('fs');
 const matcher = require('.');
 
@@ -7,20 +8,35 @@ const fixture = fs.readFileSync('fixture.txt', 'utf8');
 const paragraph = fixture.split('\n')[0];
 const sentence = fixture.split('.')[0];
 
-bench('sentence matcher test multiple patterns', () => matcher(sentence.split(' '), ['*bar', '!foo', '!*oo']));
-bench('sentence matcher test zero or more chars', () => matcher(sentence.split(' '), ['*foo']));
-bench('sentence matcher test negation', () => matcher(sentence.split(' '), ['!foo']));
-bench('sentence matcher test negation zero or more', () => matcher(sentence.split(' '), ['!*foo']));
-bench('sentence is match test', () => matcher.isMatch(sentence, '*bar*'));
+suite('Matcher', () => {
+	bench('sentence test multiple patterns', () => matcher(sentence.split(' '), ['*bar', '!foo', '!*oo']));
+	bench('sentence test zero or more chars', () => matcher(sentence.split(' '), ['*foo']));
+	bench('sentence test negation', () => matcher(sentence.split(' '), ['!foo']));
+	bench('sentence test negation zero or more', () => matcher(sentence.split(' '), ['!*foo']));
+});
 
-bench('paragraph matcher test multiple patterns', () => matcher(paragraph.split(' '), ['*bar', '!foo', '!*oo']));
-bench('paragraph matcher test zero or more chars', () => matcher(paragraph.split(' '), ['*foo']));
-bench('paragraph matcher test negation', () => matcher(paragraph.split(' '), ['!foo']));
-bench('paragraph matcher test negation zero or more', () => matcher(paragraph.split(' '), ['!*foo']));
-bench('paragraph is match test', () => matcher.isMatch(paragraph, '*bar*'));
+suite('isMatch', () => {
+	bench('sentence test', () => matcher.isMatch(sentence, '*bar*'));
+});
 
-bench('fixture matcher test multiple patterns', () => matcher(fixture.split(' '), ['*bar', '!foo', '!*oo']));
-bench('fixture matcher test zero or more chars', () => matcher(fixture.split(' '), ['*foo']));
-bench('fixture matcher test negation', () => matcher(fixture.split(' '), ['!foo']));
-bench('fixture matcher test negation zero or more', () => matcher(fixture.split(' '), ['!*foo']));
-bench('fixture is match test', () => matcher.isMatch(fixture, '*bar*'));
+suite('Matcher', () => {
+	bench('paragraph test multiple patterns', () => matcher(paragraph.split(' '), ['*bar', '!foo', '!*oo']));
+	bench('paragraph test zero or more chars', () => matcher(paragraph.split(' '), ['*foo']));
+	bench('paragraph test negation', () => matcher(paragraph.split(' '), ['!foo']));
+	bench('paragraph test negation zero or more', () => matcher(paragraph.split(' '), ['!*foo']));
+});
+
+suite('isMatch', () => {
+	bench('paragraph test', () => matcher.isMatch(paragraph, '*bar*'));
+});
+
+suite('Matcher', () => {
+	bench('fixture test multiple patterns', () => matcher(fixture.split(' '), ['*bar', '!foo', '!*oo']));
+	bench('fixture test zero or more chars', () => matcher(fixture.split(' '), ['*foo']));
+	bench('fixture test negation', () => matcher(fixture.split(' '), ['!foo']));
+	bench('fixture test negation zero or more', () => matcher(fixture.split(' '), ['!*foo']));
+});
+
+suite('isMatch', () => {
+	bench('fixture test', () => matcher.isMatch(fixture, '*bar*'));
+});
