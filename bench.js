aae9bf6a11d9485607576cbd6536696ca934d1cd
Sindre Sorhus <sindresorhus@gmail.com>
2017-02-13 14:01:56 +0700

    Cleanup #6

--- bench.js
+++ bench.js
@@ -1,6 +1,5 @@
 'use strict';
-/* global bench */
-/* global suite */
+/* global bench, suite */
 const fs = require('fs');
 const matcher = require('.');
 
@@ -8,35 +7,35 @@ const fixture = fs.readFileSync('fixture.txt', 'utf8');
 const paragraph = fixture.split('\n')[0];
 const sentence = fixture.split('.')[0];
 
-suite('Matcher', () => {
-	bench('sentence test multiple patterns', () => matcher(sentence.split(' '), ['*bar', '!foo', '!*oo']));
-	bench('sentence test zero or more chars', () => matcher(sentence.split(' '), ['*foo']));
-	bench('sentence test negation', () => matcher(sentence.split(' '), ['!foo']));
-	bench('sentence test negation zero or more', () => matcher(sentence.split(' '), ['!*foo']));
+suite('matcher() - sentence', () => {
+	bench('multiple patterns', () => matcher(sentence.split(' '), ['*bar', '!foo', '!*oo']));
+	bench('zero or more chars', () => matcher(sentence.split(' '), ['*foo']));
+	bench('negation', () => matcher(sentence.split(' '), ['!foo']));
+	bench('negation zero or more', () => matcher(sentence.split(' '), ['!*foo']));
 });
 
-suite('isMatch', () => {
-	bench('sentence test', () => matcher.isMatch(sentence, '*bar*'));
+suite('matcher.isMatch() - sentence', () => {
+	bench('sentence', () => matcher.isMatch(sentence, '*bar*'));
 });
 
-suite('Matcher', () => {
-	bench('paragraph test multiple patterns', () => matcher(paragraph.split(' '), ['*bar', '!foo', '!*oo']));
-	bench('paragraph test zero or more chars', () => matcher(paragraph.split(' '), ['*foo']));
-	bench('paragraph test negation', () => matcher(paragraph.split(' '), ['!foo']));
-	bench('paragraph test negation zero or more', () => matcher(paragraph.split(' '), ['!*foo']));
+suite('matcher() - paragraph', () => {
+	bench('multiple patterns', () => matcher(paragraph.split(' '), ['*bar', '!foo', '!*oo']));
+	bench('zero or more chars', () => matcher(paragraph.split(' '), ['*foo']));
+	bench('negation', () => matcher(paragraph.split(' '), ['!foo']));
+	bench('negation zero or more', () => matcher(paragraph.split(' '), ['!*foo']));
 });
 
-suite('isMatch', () => {
-	bench('paragraph test', () => matcher.isMatch(paragraph, '*bar*'));
+suite('matcher.isMatch() - paragraph', () => {
+	bench('test', () => matcher.isMatch(paragraph, '*bar*'));
 });
 
-suite('Matcher', () => {
-	bench('fixture test multiple patterns', () => matcher(fixture.split(' '), ['*bar', '!foo', '!*oo']));
-	bench('fixture test zero or more chars', () => matcher(fixture.split(' '), ['*foo']));
-	bench('fixture test negation', () => matcher(fixture.split(' '), ['!foo']));
-	bench('fixture test negation zero or more', () => matcher(fixture.split(' '), ['!*foo']));
+suite('matcher() - fixture', () => {
+	bench('multiple patterns', () => matcher(fixture.split(' '), ['*bar', '!foo', '!*oo']));
+	bench('zero or more chars', () => matcher(fixture.split(' '), ['*foo']));
+	bench('negation', () => matcher(fixture.split(' '), ['!foo']));
+	bench('negation zero or more', () => matcher(fixture.split(' '), ['!*foo']));
 });
 
-suite('isMatch', () => {
-	bench('fixture test', () => matcher.isMatch(fixture, '*bar*'));
+suite('matcher.isMatch() - fixture', () => {
+	bench('test', () => matcher.isMatch(fixture, '*bar*'));
 });
